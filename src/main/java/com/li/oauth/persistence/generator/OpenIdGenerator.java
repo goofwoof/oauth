705d19fff2c518edd93b6fabae4ea0dc599be05b
goofwoof <heli0086@outlook.com>
2021-05-22 22:19:14 +0800

    update-id生成方式

--- src/main/java/com/li/oauth/persistence/generator/OpenIdGenerator.java
+++ src/main/java/com/li/oauth/persistence/generator/OpenIdGenerator.java
@@ -3,18 +3,12 @@ package com.li.oauth.persistence.generator;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentityGenerator;
 import org.springframework.data.mapping.MappingException;
-import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
-@Component
 public class OpenIdGenerator extends IdentityGenerator {
     @Override
     public Serializable generate(SharedSessionContractImplementor session, Object object) throws MappingException {
-        Object id = SnowflakeIdWorker.generateId();
-        if (id != null) {
-            return (Serializable) id;
-        }
-        return super.generate(session, object);
+        return SnowflakeIdWorker.generateId();
     }
 }
